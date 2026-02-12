bdb2c39e0c4013f30edf354eac73b2de99a19d13
Circuit-Overtime <ayushbhatt633@gmail.com>
2026-02-12 07:22:07 +0000

    fixed loading of the ssh key

--- bash/script_trigger.py
+++ bash/script_trigger.py
@@ -3,9 +3,6 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-
-
-
 def deploy_reddit_post(
     reddit_data: Dict,
     vps_host: str,
@@ -67,10 +64,16 @@ def deploy_reddit_post(
             pass
         return False
 
+
+
+vps_ssh_key = os.getenv("VPS_SSH_KEY", "")
+if vps_ssh_key:
+    vps_ssh_key = vps_ssh_key.replace("\\n", "\n")
+
 deploy_reddit_post({
     "title": "Pollinations.ai update: Sage/Korpi.AI added, Polly bot hardened",
     "image": {
         "url": "https://gen.pollinations.ai/image/Cozy%208-bit%20pixel%20art%20infographic%20celebrating%204%20Pollinations%20updates.%20Headline%20in%20chunky%20pixel%20font%3A%20'POLLINATIONS%20-%20WEEKLY%20UPDATES'.%20Soft%20lime%20green%20(%23ecf874)%20and%20pastel%20gradient%20background.%20Bee%20mascot%20celebrating.%20Retro%20game%20UI%20panels%20showing%3A%20Add%20Sage%20to%20Social_Bots%2C%20Add%20Korpi.AI%20to%20Creative%2C%20%5BPATCH%5D%20Auto-deploy%20script%20for%20polly%20bot%20fixed%2C%20Harden%20Polly%20against%20prompt%20leakage%20and%20enforce%20independent%20thinking.%20Warm%20lighting%2C%20Stardew%20Valley%20vibes%2C%20mobile-readable.%20Nature%20elements%20like%20pixel%20flowers%20and%20vines.?model=nanobanana-pro&width=1024&height=1024&seed=42"
     },
 
-}, os.getenv("VPS_HOST"), os.getenv("VPS_USER"), os.getenv("VPS_SSH_KEY"))
+}, os.getenv("VPS_HOST"), os.getenv("VPS_USER"), vps_ssh_key)
