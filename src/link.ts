bf4fce33b05add097ab0f92ebdfbe425e31d4203
Circuit-Overtime <ayushbhatt633@gmail.com>
2026-03-12 15:22:43 +0000

    Deploy updated link.ts and main.ts

--- src/link.ts
+++ src/link.ts
@@ -1,3 +1,3 @@
-const LINK = "https://raw.githubusercontent.com/pollinations/pollinations/news/social/news/daily/2026-03-10/images/reddit.jpg";
-const TITLE = "Added BYOP to community apps, new Claude models, and dynamic changelogs";
+const LINK = "https://raw.githubusercontent.com/pollinations/pollinations/news/social/news/daily/2026-03-11/images/reddit.jpg";
+const TITLE = "Fixed GPT image generation by routing endpoints to West US 3";
 export {LINK, TITLE};
