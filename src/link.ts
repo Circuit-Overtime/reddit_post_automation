d8f1a0a6d7bc1c1e308df1f1020d1b295fb11706
Circuit-Overtime <ayushbhatt633@gmail.com>
2026-03-14 15:10:07 +0000

    Deploy updated link.ts and main.ts

--- src/link.ts
+++ src/link.ts
@@ -1,3 +1,3 @@
-const LINK = "https://raw.githubusercontent.com/pollinations/pollinations/news/social/news/daily/2026-03-11/images/reddit.jpg";
-const TITLE = "Fixed GPT image generation by routing endpoints to West US 3";
+const LINK = "https://raw.githubusercontent.com/pollinations/pollinations/news/social/news/daily/2026-03-13/images/reddit.jpg";
+const TITLE = "Fixed the 25h tier upgrade lag, added dirtberry-pro, and hourly Pollen refills";
 export {LINK, TITLE};
