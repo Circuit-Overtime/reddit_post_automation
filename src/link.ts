75012487d1ead771dd746377dc1cc516ff3b95fd
Circuit-Overtime <ayushbhatt633@gmail.com>
2026-04-05 18:15:55 +0000

    Deploy updated link.ts and main.ts

--- src/link.ts
+++ src/link.ts
@@ -1,3 +1,3 @@
-const LINK = "https://raw.githubusercontent.com/pollinations/pollinations/news/social/news/daily/2026-04-03/images/reddit.jpg";
-const TITLE = "SDK audio overhaul, image editing, and programmatic API keys";
+const LINK = "https://raw.githubusercontent.com/pollinations/pollinations/news/social/news/weekly/2026-04-05/images/reddit.jpg";
+const TITLE = "66 PRs merged: Audio/video upgrades, new SDK endpoints, and API key management";
 export {LINK, TITLE};
