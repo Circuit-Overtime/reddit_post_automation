6319869f1eadd6cc51c45461ca0c037c5332a660
Circuit-Overtime <ayushbhatt633@gmail.com>
2026-05-14 15:51:00 +0000

    Deploy updated link.ts and main.ts

--- src/link.ts
+++ src/link.ts
@@ -1,3 +1,3 @@
-const LINK = "https://raw.githubusercontent.com/pollinations/pollinations/news/social/news/daily/2026-05-12/images/reddit.jpg";
-const TITLE = "Merged 3 PRs: Auto top-ups, Pollenforge CLI, and Storytale";
+const LINK = "https://raw.githubusercontent.com/pollinations/pollinations/news/social/news/daily/2026-05-13/images/reddit.jpg";
+const TITLE = "Homepage refactor, FAQ cleanup, and cheaper ElevenLabs TTS";
 export {LINK, TITLE};
