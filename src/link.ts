5ba1eaefe94db53dec6b5d90d9883d648a0923bd
Circuit-Overtime <ayushbhatt633@gmail.com>
2026-05-28 16:29:36 +0000

    Deploy updated link.ts and main.ts

--- src/link.ts
+++ src/link.ts
@@ -1,3 +1,3 @@
-const LINK = "https://raw.githubusercontent.com/pollinations/pollinations/news/social/news/daily/2026-05-26/images/reddit.jpg";
-const TITLE = "We updated our docs to be readable by LLMs using scalar markdown";
+const LINK = "https://raw.githubusercontent.com/pollinations/pollinations/news/social/news/daily/2026-05-27/images/reddit.jpg";
+const TITLE = "Opened 5 flagship models to free tier, rewrote API docs generator";
 export {LINK, TITLE};
