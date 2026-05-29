0cc6f4785254a8f0338ac296cd06117b26e589e5
Circuit-Overtime <ayushbhatt633@gmail.com>
2026-05-29 16:20:50 +0000

    Deploy updated link.ts and main.ts

--- src/link.ts
+++ src/link.ts
@@ -1,3 +1,3 @@
-const LINK = "https://raw.githubusercontent.com/pollinations/pollinations/news/social/news/daily/2026-05-27/images/reddit.jpg";
-const TITLE = "Opened 5 flagship models to free tier, rewrote API docs generator";
+const LINK = "https://raw.githubusercontent.com/pollinations/pollinations/news/social/news/daily/2026-05-28/images/reddit.jpg";
+const TITLE = "Shipping phase 1 of local currency routing for Pollen checkouts";
 export {LINK, TITLE};
