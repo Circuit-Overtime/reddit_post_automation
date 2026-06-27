cafe1f879d828691963c0c00cc0f40dea3ef3af1
Circuit-Overtime <ayushbhatt633@gmail.com>
2026-06-27 15:30:29 +0000

    Deploy updated link.ts and main.ts

--- src/link.ts
+++ src/link.ts
@@ -1,3 +1,3 @@
-const LINK = "https://raw.githubusercontent.com/pollinations/pollinations/news/social/news/daily/2026-06-25/images/reddit.jpg";
-const TITLE = "Quest system merged, API docs social metadata, and auth fixes";
+const LINK = "https://raw.githubusercontent.com/pollinations/pollinations/news/social/news/daily/2026-06-26/images/reddit.jpg";
+const TITLE = "Fixed pollen top-up quest tracking and added REKTY AI bot";
 export {LINK, TITLE};
